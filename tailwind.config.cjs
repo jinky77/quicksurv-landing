01fcad823a086545394464328802f57e88668a37
Paul Levent <paullevent2014@u.northwestern.edu>
2024-11-03 02:31:07 +0100

    Added content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,13 +4,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: { primary: "#c16ce1" },
       fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
